5eee68e48c77bcd1f10f28181a97572d370f209b
MNCES <107934484+MNCES@users.noreply.github.com>
2023-12-11 11:20:50 -0300

    Add files via upload

--- src/app/noauto/noauto.page.ts
+++ src/app/noauto/noauto.page.ts
@@ -1,6 +1,9 @@
 // noauto.page.ts
 import { Component } from '@angular/core';
 import { ViajesService } from '../services/viajes.service';
+import {ViewChild, ElementRef} from '@angular/core'
+
+declare var google: any;
 
 @Component({
   selector: 'app-noauto',
@@ -13,6 +16,24 @@ export class NoautoPage {
   correo: string = '';
   origen: string = '';
   destino: string = '';
+  map: any;
+
+  @ViewChild('map', { read: ElementRef, static: false })
+  mapRef!: ElementRef;
+
+  infoWindows: any = [];
+  markers: any = [
+    {
+      title:  "Duoc UC: Sede Viña Del Mar",
+      latitude: "-33.033419363942166",
+      longitude: "-71.53314118465657"
+    },
+    {
+      title:  "Loncoche 85",
+      latitude: "-33.026571",
+      longitude: "-71.502892"
+    }
+  ];
 
   constructor(private viajesService: ViajesService) {}
 
@@ -36,12 +57,75 @@ export class NoautoPage {
   }
 
   mostrarMensaje(mensaje: string) {
-    // Aquí se puede implementar la lógica para mostrar el mensaje, por ejemplo:
+    
     console.log(mensaje);
-    // O utilizando una librería de notificaciones como ToastController
-    // this.toastController.create({
-    //   message: mensaje,
-    //   duration: 2000,
-    // }).then((toast) => toast.present());
+  }
+
+  ionViewDidEnter() {
+    this.showMap();
+  }
+
+  addMarkersToMap(markers: any[]) {
+    for (let marker of markers) {
+      let position = new google.maps.LatLng(+marker.latitude, +marker.longitude); // Parse to numbers
+      let mapMarker = new google.maps.Marker({
+        position: position,
+        title: marker.title
+      });
+
+      mapMarker.setMap(this.map);
+      this.addInfoWindowToMarker(mapMarker);
+    }
+  }
+
+  addInfoWindowToMarker(marker: any) {
+    let infoWindowContent = '<div id="content">' +
+                              '<h2 id="firstHeading" class="firstHeading">' + marker.title + '</h2>' +
+                              '<p>Latitude: ' + marker.latitude + '</p>' + 
+                              '<p>Longitude: ' + marker.longitude + '</p>' + 
+                              '<ion-button id= "navegate"> Navegate </ion-button>'
+                            '</div>';
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: infoWindowContent
+    });
+
+    marker.addListener('click', () => {
+      this.closeAllInfoWindows();
+      infoWindow.open(this.map, marker);
+
+      google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
+        const navigateButton = document.getElementById('navigate');
+        if (navigateButton) {
+          navigateButton.addEventListener('click', () => {
+            console.log("navigate button clicked!");
+            window.open('https://www.google.com/maps/dir?api=1&destination=' + marker.latitude + ',' + marker.longitude);
+          });
+        }
+      });
+    });
+
+    this.infoWindows.push(infoWindow);
+  }
+
+  closeAllInfoWindows(){
+    for(let window of this.infoWindows){
+      window.close();
+    }
+  }
+
+  showMap() {
+    if (google && google.maps) {
+      const location = new google.maps.LatLng(-33.033419363942166, -71.53314118465657);
+      const options = {
+        center: location,
+        zoom: 15,
+        disableDefaultUI: true
+      };
+      this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+      this.addMarkersToMap(this.markers);
+    } else {
+      console.error('La API de Google Maps no está disponible.');
+    }
   }
 }
